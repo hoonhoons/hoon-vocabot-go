bb73ada0d5997be757750613b2d00fecd01cc259
Park Seonghoon <hoonhoons.park@gmail.com>
2019-01-17 09:00:07 +0000

    tab

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
     //"fmt"
     "net/http"
     "github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
-	
-	"github.com/hoonhoons/hoon-vocabot-go/formats"
-	//"github.com/mongodb/mongo-go-driver/mongo"
+    "github.com/labstack/echo/middleware"
+    
+    "github.com/hoonhoons/hoon-vocabot-go/formats"
+    //"github.com/mongodb/mongo-go-driver/mongo"
 )
 
 var lastWord = map[string]formats.Word{}
@@ -19,33 +19,33 @@ var lastSentence = map[string]string{}
 
 
 func main() {
-	// Echo instance
-	
-	//fmt.Println("Hoon-vocabot has started!")
-	
-	e := echo.New()
+    // Echo instance
+    
+    //fmt.Println("Hoon-vocabot has started!")
+    
+    e := echo.New()
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+    // Middleware
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
 
     // Init
     //initQuiz()
     
-	// Routes
-	e.GET("/", hello)
-	e.POST("/quiz", generateQuiz)
-	e.GET("/quiz", sendQuiz)
-	e.POST("/check", checkQuiz)
-	e.POST("/put", putWordToWordbook)
+    // Routes
+    e.GET("/", hello)
+    e.POST("/quiz", generateQuiz)
+    e.GET("/quiz", sendQuiz)
+    e.POST("/check", checkQuiz)
+    e.POST("/put", putWordToWordbook)
 
-	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+    // Start server
+    e.Logger.Fatal(e.Start(":1323"))
 }
 
 // Handler
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+    return c.String(http.StatusOK, "Hello, World!")
 }
 
 
